384993941c5cd5cbd908c4672dbebb3cf34be3ae
Pranav Durai <pranavdurai10@gmail.com>
2023-06-02 21:56:31 +0530

    Create toffoli_gate.py
    Added Toffoli Gate implementation.

---
+++ gates/toffoli_gate.py
@@ -0,0 +1,32 @@
+'''
+///////////////////////////////////////////////////////////////////////////
+Code written by Pranav Durai for Quantum Computers on 02.06.2023 @ 21:34:23
+
+Component: Toffoli Gate
+
+Framework: Qiskit 0.43.0
+///////////////////////////////////////////////////////////////////////////
+'''
+
+# Import necessary libraries
+from qiskit import QuantumCircuit, Aer, execute
+
+# Create a quantum circuit with three qubits
+circuit = QuantumCircuit(3)
+
+# Apply the Toffoli gate
+circuit.ccx(0, 1, 2)
+
+# Measure the qubits
+circuit.measure_all()
+
+# Simulate the circuit using the local Aer simulator
+simulator = Aer.get_backend('qasm_simulator')
+job = execute(circuit, simulator, shots=1)
+
+# Get the result
+result = job.result()
+counts = result.get_counts(circuit)
+
+# Print the measurement outcome
+print("Measurement outcome:", list(counts.keys())[0])
